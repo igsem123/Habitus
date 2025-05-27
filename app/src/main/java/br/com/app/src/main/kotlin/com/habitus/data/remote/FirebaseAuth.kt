737dff56dad3b7db0c4a51ef101e28b2fde4a1b1
Joice <joice1509oliveira@gmail.com>
2025-05-26 22:09:10 -0300

    Ajustes

--- app/src/main/java/br/com/app/src/main/kotlin/com/habitus/data/remote/FirebaseAuth.kt
+++ app/src/main/java/br/com/app/src/main/kotlin/com/habitus/data/remote/FirebaseAuth.kt
@@ -1,6 +1,19 @@
 package br.com.app.src.main.kotlin.com.habitus.data.remote
 
+import com.google.firebase.Firebase
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun LoginPreview() {
+    Authenti
+}
 
 class FirebaseAuth {
 
@@ -16,6 +29,8 @@ class FirebaseAuth {
                     trySend(AuthResponde.Error(message = task.exception?.message ?: "" ))
                 }
             }
+
+        awaitClose()
     }
 
     fun loginWithEmail(email: String, password: String): Flow<AuthResponde> = callbackFlow {
@@ -27,6 +42,8 @@ class FirebaseAuth {
                     trySend(AuthResponde.Error(message = task.exception?.message ?: "" ))
                 }
             }
+
+        awaitClose()
     }
 }
 
